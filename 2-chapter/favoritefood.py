95c1feeb1d152f423d3ff068081cddf3b470cd25
annabalan <colnyshkasmile@gmail.com>
2017-07-02 19:52:03 -0700

    favoritefood

---
+++ 2-chapter/favoritefood.py
@@ -0,0 +1,8 @@
+# Favorite Foods
+
+food1 = input("What is your favorite food? ")
+food2 = input("What is your second favorite food? ")
+
+print(food1+food2)
+
+input("\n\nPress the enter key to exit.")
