a6ec0a4379e72feae73f2016494f629baeb293f0
Anna Balan <colnyshkasmile@gmail.com>
2017-07-09 21:40:30 -0700

    Update wordproblems.py

--- 2-chapter/wordproblems.py
+++ 2-chapter/wordproblems.py
@@ -1,4 +1,5 @@
 # Word Problems
+# Demonstrates numbers and math
 
 print("If a 2000 pound pregnant hippo gives birth to a 100 pound calf.")
 print("but then eats 50 pounds of food, how much does she weigh?")
