b6d3323e463312041d5ed9210d7110b79d75d943
annabalan <colnyshkasmile@gmail.com>
2017-08-13 21:10:45 -0700

    geek dictionary

--- 5-chapter/geek_translator.py
+++ 5-chapter/geek_translator.py
@@ -1,3 +1,14 @@
+# Geek Translator
+# Demonstrates using dictionaries.
+
+geek = ["404": "clueless. From the web error message 404, meaning page not found.",
+"Googling": "searching the Internet for background information on a person.",
+"Keyboard Plaque": "the collection of debris found in computer keyboards.",
+"Link Rot": "the process by which web page links become obsolete.",
+"Percussive Maintenace": "the act of striking an electronic device to make it work.",
+"Uninstalled": "being fired. Especially popular during the dot-bomb era."]
+
+
 choice = None
 while choice != "0":
   print(
@@ -16,3 +27,10 @@ while choice != "0":
   # exit
   if choice == "0":
     print("Good-bye.")
+  elif choice == "1":
+    term = input("What term do you want me to translate?: ")
+    if term in geek:
+        definition = geek[term]
+        print("\n", term, "means", definition)
+    else:
+        print("\nSorry, I don't know", term)
