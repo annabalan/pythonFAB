356a7a603ba9c83c0d676faa987a4b1bbb0e91f3
annabalan <colnyshkasmile@gmail.com>
2017-08-19 11:35:36 -0700

    hangman complete

--- 5-chapter/hangman.py
+++ 5-chapter/hangman.py
@@ -76,39 +76,48 @@ MAX_WRONG = len(HANGMAN) - 1
 WORDS = ("OVERUSED", "CLAM", "GUAM", "TAFFETA", "PYTHON")
 # initialize variables
 word = random.choice(WORDS)
-so_far = "_" * len(word)
+so_far = "-" * len(word)
 wrong = 0
 used = []
+
 print("Welcome to Hangman. Good luck!")
 
-while wrong < MAX_WRONG and so_far != word:
+while (wrong < MAX_WRONG) and (so_far != word):
     print(HANGMAN[wrong])
     print("\nYou've used the following letters:\n", used)
     print("\nSo far, the word is:\n", so_far)
+
     guess = input("\n\nEnter your guess: ")
     guess = guess.upper()
-while guess in used:
-    print("You've already guessed the letter", guess)
-    guess = input("Enter your guess: ")
-    guess = guess.upper()
+
+    while (guess in used):
+        print ("You've already guessed the letter:", guess)
+        guess = input("Enter your guess: ")
+        guess = guess.upper()
+
     used.append(guess)
-if guess in word:
-    print("\nYes!", guess, "is in word!")
-    new = ""
-    for i in range(len(word)):
-        if guess == word[i]:
-            new += guess
-        else:
-            new += so_far[i]
-            so_far = new
-else:
-    print("\nSorry,", guess, "isn't in the word.")
-    wrong += 1
-if wrong == MAX_WRONG:
+
+    if (guess in word):
+        print ("\nYes!", guess, "is in the word!")
+
+        # create a new so_far to include guess
+        new = ""
+        for i in range(len(word)):
+            if guess == word[i]:
+                new += guess
+            else:
+                new += so_far[i]
+        so_far = new
+
+    else:
+        print("\nSorry,", guess, "isn't in the word.")
+        wrong += 1
+
+if (wrong == MAX_WRONG):
     print(HANGMAN[wrong])
-    print("You've been hanged!")
+    print("\nYou've been hanged!")
 else:
     print("\nYou guessed it!")
-    print("\nThe word was", word)
 
+print("\nThe word was", word)
 input("\nPress the enter key to exit.")
