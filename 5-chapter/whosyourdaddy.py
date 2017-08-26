f3c8debf73d278d7e6ce8747c518fb8ded91c91e
annabalan <colnyshkasmile@gmail.com>
2017-08-25 17:46:57 -0700

    names added

--- 5-chapter/whosyourdaddy.py
+++ 5-chapter/whosyourdaddy.py
@@ -2,6 +2,8 @@
 
 print("\t\t\tWho's Your Daddy?")
 
-father_son = ()
+father_son = ("Bobby Adams,	Mike Adams", "Jim Adduci, Sandy Alomar",
+"Sandy Alomar Jr., Felipe Alou", "Moises Alou, Ruben Amaro", "Ruben Amaro Jr, Angel Aragon",
+"Tony Armas, Tony Armas, Jr.")
 
 name = input("Enter a male name: ")
