99617cbdd4978f83979aafe23ca2078db7386a64
annabalan <colnyshkasmile@gmail.com>
2017-08-29 20:06:43 -0700

    1

--- 5-chapter/whosyourdaddy.py
+++ 5-chapter/whosyourdaddy.py
@@ -2,9 +2,9 @@
 
 print("\t\t\tWho's Your Daddy?")
 
-father_son = ("Bobby Adams" : "Mike Adams", "Jim Adduci" : "Sandy Alomar",
-"Sandy Alomar Jr." : "Felipe Alou", "Moises Alou" : "Ruben Amaro", "Ruben Amaro Jr" : "Angel Aragon",
-"Tony Armas" : "Tony Armas, Jr.")
+father_son = {"Bobby Adams": "Mike Adams", "Jim Adduci": "Sandy Alomar",
+"Sandy Alomar Jr.": "Felipe Alou", "Moises Alou": "Ruben Amaro", "Ruben Amaro Jr": "Angel Aragon",
+"Tony Armas": "Tony Armas, Jr."}
 
 choice = None
 while choice != "0":
@@ -26,25 +26,26 @@ while choice != "0":
   elif choice == "1":
     entry = input("Enter the Son's name: ")
     if entry in father_son:
-        print("\n", entry)
+        father = father_son[entry]
+        print("\n", father)
     else:
         print("\nSorry, that name is information is not on file.")
   elif choice == "2":
       entry = input("What is the son's name?: ")
       if entry not in father_son:
-           father = input("\nWho's the father?: ")
+          father = input("\nWho's the father?: ")
           father_son[entry] = father
           print("\n", entry, "has been added.")
       else:
           print("\nThat name already exists! Try another.")
     # delete a term-definition pair
-elif choice == "3":
+  elif choice == "3":
       entry = input("What name do you want me to delete?: ")
       if entry in father_son:
         del father_son[entry]
         print("\nOkay, I deleted", entry)
       else:
         print("\nI can't do that!", entry, "doesn't exist.")
-    else:
+  else:
       print("\nSorry, but", choice, "isn't a valid choice.")
   input("\n\nPress the enter key to exit.")
