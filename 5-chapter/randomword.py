e82476ee1f4d51b7e3f4f53054e7d8ce48c8f104
annabalan <colnyshkasmile@gmail.com>
2017-09-02 21:05:55 -0700

    words

--- 5-chapter/randomword.py
+++ 5-chapter/randomword.py
@@ -4,7 +4,7 @@
 import random
 
 words = ["copper", "explain", "ill-fated", "truck", "neat", "unite", "branch", "educated",
-"tenuous", "hum", "objects", "global", "decisive", "notice", "phone", "jello", "fish"]
+"tenuous", "hum", "objects", "global", "cold", "sleep", "decisive", "notice", "phone", "jello", "fish"]
 
 while words != 0:
   high = len(words)
