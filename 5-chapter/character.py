4bf76b4d96be1a2321acd6fc7d30ebf7a1edd322
annabalan <colnyshkasmile@gmail.com>
2017-09-04 16:29:32 -0700

    character

---
+++ 5-chapter/character.py
@@ -0,0 +1,10 @@
+# Character Creator Program
+
+attributes = ("Strength", "Health", "Wisdom", "Dexterity")
+print("\t\tWelcome to Character!")
+print("You have 30 points that you can use for any character.")
+print("You can spend it on,", attributes)
+
+points = 30
+
+ 
