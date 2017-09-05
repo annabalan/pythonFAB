dfaa2a00ae0a1087fd0a758c2b6919fd38094094
annabalan <colnyshkasmile@gmail.com>
2017-09-05 11:46:27 -0700

    menu

--- 5-chapter/character.py
+++ 5-chapter/character.py
@@ -2,9 +2,14 @@
 
 attributes = ("Strength", "Health", "Wisdom", "Dexterity")
 print("\t\tWelcome to Character!")
-print("You have 30 points that you can use for any character.")
+print("You have 30 points that you can use for any of these characters.")
 print("You can spend it on,", attributes)
 
 points = 30
-
- 
+print(
+"""
+0 - Exit
+1 - Show Point Pool
+2 - Add Points To a Character
+3 - Remove Points Back To The Pool
+""")
