fd7d0d1c506b8c04ccd8733d9e2934d1b7e8082e
annabalan <colnyshkasmile@gmail.com>
2017-09-20 22:08:25 -0700

    menu loop

--- 5-chapter/character.py
+++ 5-chapter/character.py
@@ -16,25 +16,26 @@ print(
 choice = input("Choice: ")
 print()
 # exit
-if choice == "0":
-  print("Good-bye.")
-elif choice == "1":
-  print("Point Pool\n",points)
-elif choice == "2":
-  add = input("What attribute would you like to add points to?: ")
-  if add in attributes:
-      pts = input("\nHow many points would you like to add? ")
-      attributes[add] = pts
-      print("\n", add, "now has", pts)
-  else:
-      ("Sorry, but", add, "isn't a valid attriute.")
-elif choice == "3":
-    change = input("What attribute would you like to change points to?: ")
-    if change in attributes:
-        pts = input("What points would you like to add instead?: ")
-        attributes[change] = pts
-        print("\n", change, "now has", pts)
-else:
-  print("Sorry, but", choice, "isn't a valid choice.")
+while choice != "0":
+    if choice == "0":
+        print("Good-bye.")
+    elif choice == "1":
+        print("Point Pool\n",points)
+    elif choice == "2":
+        add = input("What attribute would you like to add points to?: ")
+        if add in attributes:
+            pts = input("\nHow many points would you like to add? ")
+            attributes[add] = pts
+            print("\n", add, "now has", pts)
+        else:
+            print("Sorry, but", add, "isn't a valid attriute.")
+    elif choice == "3":
+        change = input("What attribute would you like to change points to?: ")
+        if change in attributes:
+            pts = input("What points would you like to add instead?: ")
+            attributes[change] = pts
+            print("\n", change, "now has", pts)
+    else:
+        print("Sorry, but", choice, "isn't a valid choice.")
 
 input("\n\nPress the enter key to exit.")
